0f060160585dcfff6619887e3a52c08319e9602e
Danielle Donnelly <145680391+danielledonnelly@users.noreply.github.com>
2024-11-21 20:52:25 -0330

    stuck

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+console.log('Using Vite config');
+
 export default defineConfig({
   base: '/dress-up-game/',
   plugins: [react()],
